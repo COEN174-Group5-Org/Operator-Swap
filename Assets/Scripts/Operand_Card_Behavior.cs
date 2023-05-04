16a73d47e30383568f274242e10bd20051a40807
MaxWhite2024 <mjwhite@scu.edu>
2023-05-04 15:43:05 -0700

    Revert "save12 (broken)"
    This reverts commit c61e58c16525a24c7a8959aed218357ccb2a4235.

--- Assets/Scripts/Operand_Card_Behavior.cs
+++ Assets/Scripts/Operand_Card_Behavior.cs
@@ -7,11 +7,10 @@ public class Operand_Card_Behavior : MonoBehaviour
     //Numbers vars
     private Vector3 rest_position; 
     [SerializeField] private bool is_touched;
-    [SerializeField] private bool is_held;
+    [SerializeField] private bool is_following;
     [SerializeField] private float move_speed;
     [SerializeField] private float raise_dist;
     private Vector3 mouse_pos;
-    private float step;
 
     //Gameobject vars
     [SerializeField] private GameObject canvas_obj;
@@ -28,7 +27,7 @@ public class Operand_Card_Behavior : MonoBehaviour
         canvas_obj = GameObject.Find("Canvas");
         player_vals = canvas_obj.GetComponent<Player_Values>();
         is_touched = false;
-        is_held = false;
+        is_following = false;
         rest_position = GetComponent<Transform>().position;
         main_camera = GameObject.FindWithTag("MainCamera");
         card_obj = this.gameObject.transform.GetChild(0).gameObject;
@@ -41,59 +40,64 @@ public class Operand_Card_Behavior : MonoBehaviour
     void Update()
     {
         //setup step
-        step = move_speed * Time.deltaTime;
+        float step = move_speed * Time.deltaTime;
 
-        if(is_touched == true)
+        //setup mouse_pos
+        mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //cast ray from camera to mouse position
+        RaycastHit2D hit = Physics2D.Raycast(mouse_pos, Vector2.zero);
+
+        //if above ray hit this collider then,
+        if(hit.collider != null && hit.collider.gameObject == this.gameObject && player_vals.Get_Is_Holding() == false)
         {
-            Raise();
+            //Debug.Log("Touch!");
+            //set is_touched to true
+            is_touched = true;
+
+            //if left mouse button was pressed, 
+            if(Input.GetMouseButtonDown(0))
+            {
+                //set is_following to true
+                is_following = true;
+            }
         }
-        else 
+        else
         {
-            Lower();
+            //set is_touched to false
+            is_touched = false;
+
+            //set is_holding in Player_Values script to false;
+            player_vals.Set_Is_Holding(false);
         }
 
-        if(is_held == true) 
+        if(is_touched == true && is_following == false && player_vals.Get_Is_Holding() == false)
         {
-
+            //raise card
+            card_trans.position = Vector2.MoveTowards(card_trans.position, transform.position + new Vector3(0,raise_dist,0), step);;
         }
-    }
+        else if(is_touched == false && is_following == false)
+        {
+            //move card to rest position
+            card_trans.position = Vector2.MoveTowards(card_trans.position, rest_position, step);
+        }
+        else
+        {
+            //follow mouse position
+            collider_trans.position = new Vector3(mouse_pos.x, mouse_pos.y, 0);
 
-    public bool Get_Is_Touched()
-    {
-        return is_touched;
-    }
+            //set is_holding in Player_Values script to true;
+            player_vals.Set_Is_Holding(true);
 
-    public void Set_Is_Touched(bool temp)
-    {
-        is_touched = temp;
-    }
+            //if right mouse button was pressed,
+            if(Input.GetMouseButtonDown(1))
+            {
+                //set is_following to false
+                is_following = false; 
 
-    public void Set_Is_Held(bool temp)
-    {
-        is_held = temp;
-    }
-
-    public void Raise()
-    {
-        //raise card
-        card_trans.position = Vector2.MoveTowards(card_trans.position, transform.position + new Vector3(0,raise_dist,0), step);;
-    }
-
-    public void Lower()
-    {
-        //move card to rest position
-        card_trans.position = Vector2.MoveTowards(card_trans.position, rest_position, step);
-    }
-
-    public void Hold()
-    {
-        //follow mouse position
-        collider_trans.position = new Vector3(mouse_pos.x, mouse_pos.y, 0);
-    }
-
-    public void Put_Back()
-    {
-        //reset position
-        collider_trans.position = rest_position;
+                //reset position
+                collider_trans.position = rest_position;
+            }
+        }
     }
 }
--- Assets/Scripts/Player_Values.cs
+++ Assets/Scripts/Player_Values.cs
@@ -9,34 +9,10 @@ public class Player_Values : MonoBehaviour
     [SerializeField] public int num_operands_in_deck = 3;
     [SerializeField] public int current_turn = 0;
     [SerializeField] public bool is_holding_card = false;
-    private Vector3 mouse_pos;
 
     public void Update()
     {
-        //setup mouse_pos
-        mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        //cast ray from camera to mouse position
-        RaycastHit2D hit = Physics2D.Raycast(mouse_pos, Vector2.zero);
-
-        //if above ray hit a card collider then,
-        if(hit.collider != null && hit.collider.gameObject.gameObject.layer == 6 && is_holding_card == false)
-        {
-            //Debug.Log("Touch!");
-            //set is_touched to true
-            hit.collider.gameObject.GetComponent<Operand_Card_Behavior>().Set_Is_Touched(true);
-
-            //if left mouse button was pressed, 
-            if(Input.GetMouseButtonDown(0))
-            {
-                //set is_following to true
-                hit.collider.gameObject.GetComponent<Operand_Card_Behavior>().Set_Is_Touched(false);
-                hit.collider.gameObject.GetComponent<Operand_Card_Behavior>().Set_Is_Held(true);
-                is_holding_card = true;
-            }
-        }
-
-        if(Input.GetMouseButtonDown(1))
+        //
     }
 
     //Move to next turn
