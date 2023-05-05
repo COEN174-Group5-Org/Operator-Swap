0d40fd33fd69986bdf8c5ee118298a446e2c351d
Zekaria Esa <zekaria.esa@scholar.smash.org>
2023-05-04 17:08:12 -0700

    create evaluate button and connecting script

---
+++ Assets/Scripts/Evaluate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evaluate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void evaluate()
+    {
+        Debug.Log("pressed button");
+    }
+}
