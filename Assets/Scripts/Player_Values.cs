647e3a02190d3910e0c3425c692c7493b0e8c372
MaxWhite2024 <118154938+MaxWhite2024@users.noreply.github.com>
2023-05-14 14:40:27 -0700

    Hand slots (#7)
    * save1
    
    * save2
    
    * Revert "Merge branch 'main' into Hand-Slots"
    
    This reverts commit 5180249f093848bc9b7ed867ca6de9ee63a6d1b2, reversing
    changes made to 1e25bacd90c12ed5995090e9d1b6dabf83475667.
    
    * save1
    
    * save2
    
    * save3
    
    * save4
    
    * save3 I think
    
    * save4
    
    * save5 (cards/slots working!)
    
    * save6 (Operator cards added)
    
    * save7 (Card kinda broken)
    
    * broken1
    
    * Revert "broken1"
    
    This reverts commit 29ad95835d738e4f11126005fc6b29c6e49623ff.
    
    * save8
    
    * save9 (cards work, slots don't)
    
    * save10 (cards work, slots almost work)
    
    * save11 (cards work, slots work!!!)
    
    * Revert "Merge branch 'main' into Hand-Slots"
    
    This reverts commit 5f33b174e3643a7d90cf17aef37b020abd9da9bd, reversing
    changes made to 86b5814f4c6341bcbb77763462d23fba91bb0e39.

--- Assets/Scripts/Player_Values.cs
+++ Assets/Scripts/Player_Values.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 //Stores all the variables and functions of the current player
 public class Player_Values : MonoBehaviour
 {
-    [SerializeField] public string[] player_operators = {"+", "*"};
-    [SerializeField] public int num_operands_in_deck = 3;
-    [SerializeField] public int current_turn = 0;
-    [SerializeField] public bool is_holding_card = false;
+    //player_operators stores the operators that will appear in the player's hand at the start of a turn
+    [SerializeField] public string[] player_operators = {"+", "*"}; 
+
+    //num_operands_in_deck stores the number of operand card that will appear in the player's hand at the start of a turn
+    [SerializeField] public int num_operands_in_deck = 3; 
+
+    //current_turn stores the current turn of the scene
+    [SerializeField] public int current_turn = 0; 
+
+    //is_holding_card stores whether or not the player is holding a card
+    [SerializeField] public bool is_holding_card = false; 
+
+    //held_card stores the GameObject representing the last held card
+    public GameObject held_card; 
+
+    //held_card_type stores the type of the currently held card (should only be either "none", "operand", or "operator")
+    [SerializeField] public string held_card_type = "none";
 
     //Move to next turn
     public void Next_turn()
@@ -22,7 +35,7 @@ public class Player_Values : MonoBehaviour
         return player_operators;
     }
 
-   public  void Set_Player_Operators(string[] new_ops)
+    public  void Set_Player_Operators(string[] new_ops)
     {
         player_operators = new_ops;
     }
@@ -45,5 +58,25 @@ public class Player_Values : MonoBehaviour
     public void Set_Is_Holding(bool new_bool)
     {
         is_holding_card = new_bool;
+    }
+
+    public GameObject Get_Held_Card()
+    {
+        return held_card;
+    }
+
+    public void Set_Held_Card(GameObject new_obj)
+    {
+        held_card = new_obj;
+    }
+
+    public string Get_Held_Card_Type()
+    {
+        return held_card_type;
+    }
+
+    public void Set_Held_Card_Type(string new_string)
+    {
+        held_card_type = new_string;
     }
 }
