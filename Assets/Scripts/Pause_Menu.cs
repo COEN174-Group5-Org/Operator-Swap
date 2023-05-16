dbef0542f21db18aa28be00b85dad875038196fb
SMak8 <smak@scu.edu>
2023-05-15 21:09:51 -0700

    may15

--- Assets/Scripts/Pause_Menu.cs
+++ Assets/Scripts/Pause_Menu.cs
@@ -1,3 +1,5 @@
+// Even though this is the "Pause_Menu" Script, it's really the canvas overlay script
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +8,28 @@ using UnityEngine.SceneManagement;
 public class Pause_Menu : MonoBehaviour
 {
     public static bool GameIsPaused = false; 
+    public static bool GameIsOver = false; // when success or defeat is triggered, set this boolean to true
     // to use this boolean: 
     // if(Pause_Menu.GameIsPaused){ ... }
 
     public GameObject pauseMenuUI;
     public GameObject pauseButton; 
+    public GameObject successScreenUI;
+    public GameObject defeatScreenUI;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        
+        if (!GameIsOver)
         {
-            if(GameIsPaused)
-            {
-                Resume();
-            }else{
-                Pause();
+            if(Input.GetKeyDown(KeyCode.Escape)){
+                if(GameIsPaused)
+                {
+                    Resume();
+                }else{
+                    Pause();
+                }
             }
         }
     }
@@ -47,6 +55,20 @@ public class Pause_Menu : MonoBehaviour
         Time.timeScale = 1f;
         //Debug.Log("Loading menu...");
         SceneManager.LoadScene("Title_menu");
+    }
+
+    public void success()
+    {
+        GameIsOver = true; 
+        pauseButton.SetActive(false);
+        successScreenUI.SetActive(true);
+    }
+
+    public void defeat()
+    {
+        GameIsOver = true; 
+        pauseButton.SetActive(false);
+        defeatScreenUI.SetActive(true);
     }
 
 }
