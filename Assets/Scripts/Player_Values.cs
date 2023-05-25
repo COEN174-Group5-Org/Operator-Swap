c445c9be4c1eb8717d3eb92d99562f560d42074b
MaxWhite2024 <mjwhite@scu.edu>
2023-05-25 15:53:28 -0700

    save 5/25/2023 3:53

--- Assets/Scripts/Player_Values.cs
+++ Assets/Scripts/Player_Values.cs
@@ -6,22 +6,51 @@ using UnityEngine;
 public class Player_Values : MonoBehaviour
 {
     //player_operators stores the operators that will appear in the player's hand at the start of a turn
-    [SerializeField] public string[] player_operators = {"+", "*"}; 
+    //[SerializeField] public string[] player_operators; 
+
+    //operand_upper_bound stores the upper bound of the operand range and is changed by the selected difficulty
+    private int operand_upper_bound;
 
     //num_operands_in_deck stores the number of operand card that will appear in the player's hand at the start of a turn
-    [SerializeField] public int num_operands_in_deck = 3; 
+    public int num_operands_in_deck; 
 
     //current_turn stores the current turn of the scene
-    [SerializeField] public int current_turn = 0; 
+    public int current_turn = 0; 
 
     //is_holding_card stores whether or not the player is holding a card
-    [SerializeField] public bool is_holding_card = false; 
+    public bool is_holding_card = false; 
 
-    //held_card stores the GameObject representing the last held card
+    //held_card stores the GameObject representing the LAST held card
     public GameObject held_card; 
 
     //held_card_type stores the type of the currently held card (should only be either "none", "operand", or "operator")
-    [SerializeField] public string held_card_type = "none";
+    public string held_card_type = "none";
+
+    /*** Player hand variables ***/
+    //...
+    [SerializeField] private int[] hand_operands;
+
+    //hand_operand_objs ...
+    //These values must be set from the inspector!
+    [SerializeField] private GameObject[] hand_operand_objs;
+
+    //...
+    private string[] hand_operators;
+
+    //hand_operator_objs ...
+    //These values must be set from the inspector!
+    [SerializeField] private GameObject[] hand_operator_objs;
+
+    //...
+    private GameObject[] battle_objects;
+
+    //...
+    private string[] battle_equation;
+
+    void Start()
+    {
+        Generate_Hand_For_3_Operands();
+    }
 
     //Move to next turn
     public void Next_turn()
@@ -29,16 +58,48 @@ public class Player_Values : MonoBehaviour
         current_turn++;
     }
 
-    //return the current player operators
-    public string[] Get_Player_Operators()
+    //generate hand for level with 3 operands
+    public void Generate_Hand_For_3_Operands()
     {
-        return player_operators;
+        //reset current turn
+        current_turn = 0;
+
+        //reset is_holding_card
+        is_holding_card = false;
+
+        //set number of operands to 3
+        num_operands_in_deck = 3;
+
+        //set hand_operands
+        hand_operands = new int[3];
+        for(int i = 0; i < 3; i++)
+            hand_operands[i] = (int) Random.Range(1f, 9.999999f); 
+
+        //set hand_operators
+        hand_operators = new string[2];
+
+        //reset battle objects
+        battle_objects = new GameObject[5];
+
+        //make equation "empty"
+        battle_equation = new string[5] {"EMPTY","EMPTY", "EMPTY", "EMPTY", "EMPTY"};
     }
 
-    public  void Set_Player_Operators(string[] new_ops)
+    //generate hand for level with 4 operands
+    public void Generate_Hand_For_4_Operands()
     {
-        player_operators = new_ops;
+        num_operands_in_deck = 4;
     }
+
+    // public string[] Get_Player_Operators()
+    // {
+    //     return player_operators;
+    // }
+
+    // public  void Set_Player_Operators(string[] new_ops)
+    // {
+    //     player_operators = new_ops;
+    // }
 
     public int Get_Num_Operands()
     {
