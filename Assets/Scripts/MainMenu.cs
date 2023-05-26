9fd9de2194589ec48315a1198a98e23f81cd4b77
FizzyBubblez <alvinbl2002@gmail.com>
2023-05-26 09:38:42 -0700

    Added transitions

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,32 @@ using UnityEngine.SceneManagement; // Allows for the changing of scenes
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     // Function is called whenever the play button is pressed
     public void PlayGame ()
-    {
+    {       
         // Load the next scene of the game
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); 
     }
 
-    public void QuitGame ()
+    IEnumerator LoadLevel(int levelIndex)
     {
-        Debug.Log("QUIT!");
-        Application.Quit();
+        //Play animation
+        transition.SetTrigger("Start");
+
+        //Wait
+        yield return new WaitForSeconds(transitionTime);
+
+        //Load scene
+        SceneManager.LoadScene(levelIndex);
     }
+
+    // public void QuitGame ()
+    // {
+    //     Debug.Log("QUIT!");
+    //     Application.Quit();
+    // }
 }
